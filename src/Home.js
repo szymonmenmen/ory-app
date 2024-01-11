08d7596bf47fac6f3429c5833dd52e33bb283749
Szymon Mendla <s.mendla@emporix.com>
2024-01-11 11:30:04 +0100

    apigee approach

--- src/Home.js
+++ src/Home.js
@@ -50,7 +50,7 @@ function Home() {
     localStorage.setItem("REDIRECT_URL", redirectUrl);
 
     // None
-    window.location.href = `https://strange-mayer-6ttl9at1pm.projects.oryapis.com/oauth2/auth?response_type=code&client_id=3521db1d-d51d-46a5-97bd-af364ee07f37&redirect_uri=${redirectUrl}&state=${state}&scope=openid%20user:email%20email%20profile%20first_name%20userinfo.profile%20last_name&code_challenge_method=S256&code_challenge=${code_challenge}`;
+    window.location.href = `https://strange-mayer-6ttl9at1pm.projects.oryapis.com/oauth2/auth?response_type=code&client_id=3521db1d-d51d-46a5-97bd-af364ee07f37&redirect_uri=${redirectUrl}&state=${state}&scope=openid%20user:email%20email%20profile%20first_name%20https://www.googleapis.com/auth/userinfo.profile%20https://www.googleapis.com/auth/userinfo.email%20last_name&code_challenge_method=S256&code_challenge=${code_challenge}`;
 
     // Machine to machine
     // window.location.href = `https://strange-mayer-6ttl9at1pm.projects.oryapis.com/oauth2/auth?response_type=code&client_id=93b0c7f6-25ab-48bb-9e6f-51122dc6f5d7&redirect_uri=${redirectUrl}&state=${state}&scope=openid%20offline_access%20user:email%20email%20profile`;
